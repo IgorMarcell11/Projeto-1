90374580832e62a8be6a2f6790d54fbdc08655e4
Igor Marcell Passos <igormarcell804@gmail.com>
2024-08-25 21:06:14 -0300

    Criação de script.js para controle do formulário

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const btn = document.querySelector('#submitbtn')
+
+const texto_inicial = document.querySelector('#texto_boasvindas')
+const previsao = document.querySelector('#texto_previsao')
+
+
+btn.addEventListener("click", function(event) {
+    event.preventDefault()
+
+    const nome = document.querySelector('#nome').value
+    const cidade = document.querySelector('#escolha_cidade').value
+    
+    texto_inicial.innerHTML = "Olá, " + nome + " de " + cidade
+    previsao.innerHTML = "Esta é a previsão do tempo para " + cidade + " nesta semana." 
+})
+
+function cardPrevisao(){}
