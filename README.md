3e269efc17aacb515f23fe79683ac862d45b5137
Igor Marcell Passos <igormarcell804@gmail.com>
2024-08-25 21:21:31 -0300

    alterações no readme e no style.css

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Projeto-1
 Projeto 1 - Referente a 3ª semana da trilha Desenvolvimento Fullstack da ResTIC36
 
+INCOMPLETO
+
 ## Descrição do Projeto
+A partir da cidade selecionada pelo usuário, a página retorna a previsão do tempo na para a semana corrente.
 
 ## Instruções
+Insira o nome, em seguida selecione uma das cidades cadastradas
 
 ## Tecnologias Utilizadas
+Html5, CSS e JavaScript
 
 ## Possíveis Melhorias
+A utilização de uma API meteorológica traria a previsão do tempo mais detalhada e atualizada.
+
--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const btn = document.querySelector('#submitbtn')
 const texto_inicial = document.querySelector('#texto_boasvindas')
 const previsao = document.querySelector('#texto_previsao')
 
-
 btn.addEventListener("click", function(event) {
     event.preventDefault()
 
@@ -13,5 +12,3 @@ btn.addEventListener("click", function(event) {
     texto_inicial.innerHTML = "Olá, " + nome + " de " + cidade
     previsao.innerHTML = "Esta é a previsão do tempo para " + cidade + " nesta semana." 
 })
-
-function cardPrevisao(){}
